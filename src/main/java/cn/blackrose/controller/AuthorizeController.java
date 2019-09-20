590d323066be11a5c73e9b562447cec93b8117c9
blackrose-bk <qkun233@163.com>
2019-09-20 10:14:06 +0800

    extract github config from code to properties

---
+++ src/main/java/cn/blackrose/controller/AuthorizeController.java
@@ -0,0 +1,41 @@
+package cn.blackrose.controller;
+
+import cn.blackrose.dto.AccessTokenDTO;
+import cn.blackrose.dto.GithubUser;
+import cn.blackrose.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.client.url}")
+    private String redirectUrl;
+    @GetMapping("/callback")
+
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state) throws IOException {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_url(redirectUrl);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
