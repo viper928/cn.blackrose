a137ee1433c1e88381d0c2c0d462cc08f1cdace6
blackrose-bk <qkun233@163.com>
2019-09-21 22:11:58 +0800

    add check cookies

--- src/main/java/cn/blackrose/controller/AuthorizeController.java
+++ src/main/java/cn/blackrose/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package cn.blackrose.controller;
 
 import cn.blackrose.dto.AccessTokenDTO;
 import cn.blackrose.dto.GithubUser;
+import cn.blackrose.mapper.UserMapper;
+import cn.blackrose.model.User;
 import cn.blackrose.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,7 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -23,10 +28,15 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.client.url}")
     private String redirectUrl;
+    
+    @Autowired
+    private UserMapper userMapper;
+    
     @GetMapping("/callback")
 
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state) throws IOException {
+                           @RequestParam(name="state") String state,
+                           HttpServletResponse response) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_url(redirectUrl);
@@ -34,8 +44,21 @@ public class AuthorizeController {
         accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+
+        if(githubUser !=null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
+            return "redirect:/";
+        }else{
+            return "redirect:/";
+        }
     }
 }
--- src/main/java/cn/blackrose/controller/IndexController.java
+++ src/main/java/cn/blackrose/controller/IndexController.java
@@ -1,13 +1,35 @@
 package cn.blackrose.controller;
 
+import cn.blackrose.mapper.UserMapper;
+import cn.blackrose.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @RequestMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie:cookies){
+            if(("token").equals(cookie.getName())){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 }
